eff7214f8b9ee01001f94ad4046aaaa6353c485d
macintoshhelper <macintoshhelper@users.noreply.github.com>
2017-05-01 16:49:00 +0300

    Create gulpfile.js

---
+++ settingup/gulp/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+
+gulp.task('default', () => {
+  /*
+   * Gulp task goes here
+   */
+});
+
+
+gulp.start('default');
