6e95935d71c82e74c98ec3d8d8be90d8eacb3a4a
macintoshhelper <macintoshhelper@users.noreply.github.com>
2017-05-01 16:19:04 +0300

    Create gulpfile.js

---
+++ settingup/babel/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+
+gulp.task('default', () => {
+  gulp.src('client-build/src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env'],
+    }))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/assets/js'));
+});
+
+
+gulp.start('default');
